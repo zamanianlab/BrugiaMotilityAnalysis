e3684d32ec7e654d6c784a152e7dc46dd224605d
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2018-04-26 16:46:50 -0500

    Completed functions. Works for full test video.

--- dense-flow-cv.py
+++ dense-flow-cv.py
@@ -4,10 +4,6 @@ from pathlib import Path
 import imageio
 import matplotlib.pyplot as plt
 from skimage.filters import threshold_local
-from skimage.feature import peak_local_max
-from skimage.morphology import watershed
-from scipy import ndimage
-# from skimage.filters import threshold_otsu
 
 
 def optical_flow(vid):
@@ -25,8 +21,8 @@ def optical_flow(vid):
     hsv[..., 1] = 255
 
     # Define the codec and create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
-    avi = cv2.VideoWriter('A0003_OF_15.avi', fourcc, 7.0, (1920, 1080))
+    # fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    # avi = cv2.VideoWriter('A0003_OF_15.avi', fourcc, 7.0, (1920, 1080))
 
     # txt = open('A0001_OF_15.txt', 'ab')
     # bin = open('A0001_OF_15.bin', 'ab')
@@ -64,33 +60,32 @@ def optical_flow(vid):
             # and next frames
             all_mag[count] = mag
             count += 1
-            # elif count == length - 1:
-            if count > 4:
-                break
+        else:
+            break
 
-        print("all_mag shape is: {}".format(all_mag.shape))
-        sum = np.sum(all_mag, axis=0)
-        mean = np.mean(all_mag, axis=0)
-        print("shape of sum is: {}".format(sum.shape))
-        print("total movement is: {}".format(np.sum(sum)))
-        print("the lowest movement value in sum is: {}".format(np.amin(sum)))
-        print("the maximum movement value in sum is: {}".format(np.amax(sum)))
+    print("all_mag shape is: {}".format(all_mag.shape))
+    sum = np.sum(all_mag, axis=0)
+    mean = np.mean(all_mag, axis=0)
+    print("shape of sum is: {}".format(sum.shape))
+    print("total movement is: {}".format(np.sum(sum)))
+    print("the lowest movement value in sum is: {}".format(np.amin(sum)))
+    print("the maximum movement value in sum is: {}".format(np.amax(sum)))
 
-        txt = wd.joinpath('A0003_OF_15_sum.txt')
-        sum_png = wd.joinpath('A0003_OF_15_sum.png')
-        mean_png = wd.joinpath('A0003_OF_15_mean.png')
+    txt = wd.joinpath('test_video_OF_15_sum.txt')
+    sum_png = wd.joinpath('test_video_OF_15_sum.png')
+    mean_png = wd.joinpath('test_video_OF_15_mean.png')
 
-        # write to text
-        with txt.open(mode='wb') as file:
-            np.savetxt(file, sum, fmt='%0.2e')
+    # write to text
+    with txt.open(mode='wb') as file:
+        np.savetxt(file, sum, fmt='%0.2e')
 
-        # write to png
-        # uint_sum = imageio.core.image_as_uint(sum)
-        imageio.imwrite(sum_png, sum)
-        imageio.imwrite(mean_png, mean)
+    # write to png
+    # uint_sum = imageio.core.image_as_uint(sum)
+    imageio.imwrite(sum_png, sum)
+    imageio.imwrite(mean_png, mean)
 
-        cap.release()
-        cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 def segment_worms(vid):
@@ -100,6 +95,9 @@ def segment_worms(vid):
     height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     array = np.zeros((length, height, width))
+    # slice for testing
+    array = array[0:5, :, :]
+    length = len(array)
 
     count = 0
 
@@ -118,7 +116,6 @@ def segment_worms(vid):
     # calculate difference between each frame and max intensity
     difference_array = np.zeros((length, height, width))
     threshold_array = np.zeros((length, height, width))
-    binary_array = np.zeros((length, height, width))
     count = 0
     # loop to perform segmentation for every frame (uncomment when finished
     # testing)
@@ -126,57 +123,42 @@ def segment_worms(vid):
         difference = abs(np.subtract(frame, max_intensity))
         difference_array[count] = difference
         block_size = 21
-        scikit_threshold = threshold_local(difference, block_size, offset=-160)
-        threshold_array[count] = scikit_threshold
-        # binary = difference >= threshold
-        # binary_array[count] = binary
+        threshold = threshold_local(difference, block_size, offset=-160)
+        threshold_array[count] = threshold
         count += 1
 
-    # sckit-image method for local thresholding
-    # difference = abs(np.subtract(array[0], max_intensity))
-    # block_size = 21
-    # threshold = threshold_local(difference, block_size, offset=-160)
-    # binary = difference >= threshold
-
-    # opencv2 watershed threshold
-    for threshold in threshold_array:
-        dist = ndimage.distance_transform_edt(threshold)
-        localMax = peak_local_max(dist, indices=False, min_distance=20,
-                                  labels=threshold)
-        markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
-        labels = watershed(-dist, markers, mask=threshold)
-        print("[INFO] {} unique segments found".format(
-            len(np.unique(labels)) - 1))
-
-    # fig, axes = plt.subplots(nrows=6, figsize=(7, 7))
+    # for plotting/testing
+    # fig, axes = plt.subplots(nrows=3, figsize=(10, 10))
     # ax = axes.ravel()
     # plt.gray()
     #
     # ax[0].imshow(max_intensity)
     # ax[0].set_title('Max Project')
     #
-    # ax[1].imshow(array[0])
-    # ax[1].set_title('Frame 0')
+    # ax[1].imshow(difference_array[0])
+    # ax[1].set_title('Difference')
     #
-    # ax[2].imshow(difference_array[0])
-    # ax[2].set_title('Diff')
-    #
-    # ax[3].imshow(threshold_array[0])
-    # ax[3].set_title('skimage Threshold')
-    #
-    # ax[4].imshow(cv_threshold)
-    # ax[4].set_title('CV Threshold')
-    #
-    # ax[5].imshow(markers[0])
-    # ax[5].set_title('markers')
+    # ax[2].imshow(threshold_array[0])
+    # ax[2].set_title('Threshold')
     #
     # for a in ax:
     #     a.axis('off')
     #
     # plt.show()
+    print('Finished thresholding.')
+    return threshold_array
 
-wd = Path.home().joinpath('Desktop', 'temp_testing')
+
+def normalization(array):
+    array.astype('float')[array == 0] = 'NaN'
+    sum = np.sum(array, axis=0)
+    total_sum = np.sum(sum)
+    print("Normalization factor is: {}.".format(total_sum))
+
+wd = Path.home().joinpath('Box Sync', 'GitHub', 'BrugiaMotilityAnalysis')
 # A0003b.avi is a AVI from WormViz comverted to grayscale in Fiji
-vid = wd.joinpath('A0003b.avi')
-# optical_flow(vid)
-segment_worms(vid)
+vid = wd.joinpath('test_video.avi')
+
+optical_flow(vid)
+threshold_array = segment_worms(vid)
+normalization(threshold_array)
