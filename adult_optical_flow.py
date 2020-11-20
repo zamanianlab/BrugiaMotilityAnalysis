90e5d90627614f50fed0044857e1fbd4cc745541
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2020-11-20 14:00:07 -0600

    adult changes

--- adult_optical_flow.py
+++ adult_optical_flow.py
@@ -3,13 +3,13 @@ import numpy as np
 from scipy import ndimage
 from pathlib import Path
 import imageio
-# import matplotlib.pyplot as plt
-# from skimage.filters import threshold_local
 from skimage.filters import threshold_otsu
-# from skimage import morphology
 from datetime import datetime
 import csv
 import argparse
+# from skimage import morphology
+# import matplotlib.pyplot as plt
+# from skimage.filters import threshold_local
 
 
 def optical_flow(wd, vid):
